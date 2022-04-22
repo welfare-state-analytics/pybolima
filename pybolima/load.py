cd2d2fcbfac8c122320466e9f9a97e3354cb51ac
roger-mahler <roger.mahler@hotmail.com>
2022-04-22 12:36:06 +0000

    Handle load of different formats

--- pybolima/load.py
+++ pybolima/load.py
@@ -1,16 +1,41 @@
 from __future__ import annotations
 
 from typing import Iterable
+
 import pandas as pd
+
+
+def read_header(filename: str) -> str:
+    with open(filename, "r", encoding='utf-8') as fp:
+        return fp.readline()
 
 
 def load_bolima(filename: str) -> pd.DataFrame:
 
-    corpus: pd.DataFrame = pd.read_parquet(filename)
-    corpus['document_name'] = corpus['title'] + "_" + corpus.page.astype(str)
-    corpus['issue_name'] = corpus['title']
-    corpus['document_id'] = corpus.index
-    corpus['year'] = corpus.title.str.split("-").str[1].str[:4].astype(int)
+    corpus: pd.DataFrame
+
+    if filename.endswith("feather"):
+        corpus = pd.read_feather(filename)
+
+    elif filename.endswith("parquet"):
+        corpus: pd.DataFrame = pd.read_parquet(filename)
+
+    else:
+        header = read_header(filename)
+        sep = '\t' if header.count('\t') > 0 else ','
+        corpus = pd.read_csv(filename, sep=sep)
+
+    expected_columns: set[str] = {'title', 'page', 'text'}
+
+    if set(corpus.columns).intersection(expected_columns) != expected_columns:
+        raise ValueError(f"column(s) not found: {expected_columns-set(corpus.columns)}")
+
+    if 'document_name' not in corpus.columns:
+
+        corpus['document_name'] = corpus['title'] + "_" + corpus.page.astype(str)
+        corpus['issue_name'] = corpus['title']
+        corpus['document_id'] = corpus.index
+        corpus['year'] = corpus.title.str.split("-").str[1].str[:4].astype(int)
 
     return corpus
 
@@ -20,4 +45,5 @@ def issue_reader(source: str | pd.DataFrame) -> Iterable[tuple[str, pd.DataFrame
     titles: list[str] = sorted(corpus['title'].unique())
     for title in titles:
         pages: pd.DataFrame = corpus[corpus['title'] == title]
+        pages.drop(columns=['Unnamed: 0'], errors='ignore', inplace=True)
         yield (title, pages)
